138a1a61904ca8527d10fde256eac10d55680b7c
akbarrahmatm <akbarrahmatmulyatama@gmail.com>
2023-01-17 21:02:47 +0700

    Latest update

---
+++ app/src/main/java/com/akbarrahmatm/projectuas_2112500851/DirectionMobilActivity.java
@@ -0,0 +1,14 @@
+package com.akbarrahmatm.projectuas_2112500851;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DirectionMobilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction_mobil);
+    }
+}
---
+++ app/src/main/java/com/akbarrahmatm/projectuas_2112500851/direction/MobilActivity.java
@@ -0,0 +1,14 @@
+package com.akbarrahmatm.projectuas_2112500851.direction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MobilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobil);
+    }
+}
